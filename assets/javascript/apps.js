acacdd54cffecb5a850e3a53910617709a9bec53
Lysnandie <30992505+Lysnandie@users.noreply.github.com>
2017-12-08 22:02:53 -0600

    gifs

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -1,77 +1,88 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  //arary of tvshows
-  var shows = ["Martin", "Empire", "Fresh Prince", "Scandal", "Queen Sugar", "Being Mary Jane", "Family Matters"];
+      //arary of tvshows
+      var shows = ["Martin", "Empire", "Fresh Prince", "Scandal", "Queen Sugar", "Being Mary Jane", "Family Matters"];
 
-  //make buttons
-  function renderButtons() {
-    //clears any items inside before looping through
-    $("#gifs-here").empty();
+      //make buttons
+      function renderButtons() {
+        //clears any items inside before looping through
+        $("#myButtons").empty();
 
-    //loop through array and make buttons
-    for (var i = 0; i < shows.length; i++) {
-      //create button using JQuery for each item in array
-      var a = $("<button>");
+        //loop through array and make buttons
+        for (var i = 0; i < shows.length; i++) {
+          //create button using JQuery for each item in array
+          var a = $("<button>");
 
-      //create class with JQuery
-      a.addClass("show");
+          //create class with JQuery
+          a.addClass("show");
 
-      //attribute
-      a.attr("data-name", shows[i]);
+          //attribute
+          a.attr("data-name", shows[i]);
 
-      //text for button
-      a.text(shows[i]);
+          //text for button
+          a.text(shows[i]);
 
-      //add buttons to gif div
-      $("#gifs-here").append(a);
-    }
-  }
-
-//get user input
-  $("#submit").on("click", function(event){
-    //stops function from refreshing
-    event.preventDefault();
-    //grabs user input
-    var show = $("#show-input").val().trim();
-    //adds shows from textbox into TvShows array
-    shows.push(show);
-    //calls our renderButtons function
-    renderButtons();
-
-});
-
-function gifs() {
-  var tvShows = $(this).attr("data-name");
-
-  //giphyURL
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + shows + "&api_key=0vhMQ7uc2Ur2tZjwl9A4bTLywDCAaxpo&limit=10";
-
-
-  //ajax call to get gif from site
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-    }).done(function(response){
-
-      var results = response.data;
-
-      for (var i=0; i < results.length; i++) {
-
-        var showDiv = $("<div>");
-
-        var rating = results [i].rating;
-
-        var p = $("<p>").text("rating: " + rating);
-
-        var showImage = $("<img>");
-
-        showImage.attr("src", results[i].images.fixed_height.url);
-
-        showDiv.append(p);
-        showDiv.append(showImage);
-
-        $("#gifs-here").prepend(showDiv);
+          //add buttons to gif div
+          $("#myButtons").append(a);
+        }
       }
-    });
-  }
+
+      //get user input
+      $("#submit").on("click", function(event) {
+        //stops function from refreshing
+        event.preventDefault();
+        //grabs user input
+        var show = $("#show-input").val().trim();
+        //adds shows from textbox into TvShows array
+        shows.push(show);
+        //calls our renderButtons function
+        renderButtons();
+
+      });
+
+      renderButtons();
+
+      //clears gifs-here div
+
+      $(".show").click (function (){
+            $("#gifs-here").empty();
+      }
+);
+
+
+      $(document.body).on("click", ".show", function() {
+          var tvShows = $(this).attr("data-name");
+
+          //giphyURL
+          var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + tvShows + "&api_key=0vhMQ7uc2Ur2tZjwl9A4bTLywDCAaxpo&limit=10";
+
+
+          //ajax call to get gif from site
+          $.ajax({
+            url: queryURL,
+            method: "GET"
+          }).done(function(response) {
+
+            var results = response.data;
+
+            for (var i = 0; i < results.length; i++) {
+
+              var showDiv = $("<div>");
+
+              var rating = results[i].rating;
+
+              var p = $("<p>").text("rating: " + rating);
+
+              var showImage = $("<img>");
+
+              showImage.attr("src", results[i].images.fixed_height.url);
+
+              showDiv.append(p);
+              showDiv.append(showImage);
+
+              $("#gifs-here").prepend(showDiv);
+            }
+          });
+
+      });
 });
